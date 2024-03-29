ca99a6dd4056ce6d162efbe4f703d4e0ac542f2e
Jose-leandro <lenadrojose124leandro@gmail.com>
2024-03-29 18:05:57 -0300

    feat: Implementação da funcionalidade de alternância entre anotações e listas
    Este commit adiciona a capacidade de alternar entre anotações e listas dentro da aplicação. Um mecanismo de seleção foi desenvolvido para permitir que os usuários alternem entre essas duas visualizações de forma transparente. Esta funcionalidade aprimora a experiência do usuário ao fornecer flexibilidade na visualização de diferentes tipos de conteúdo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,18 @@
-import React, { type ChangeEvent, useState, useEffect, useRef } from 'react'
-import { NoteCard } from './components/note-card'
-import { NewNoteCard } from './components/new-note-card'
+import React, { useState, useRef } from 'react'
 import './styles/reset.sass'
 import './styles/gobal.sass'
 import './styles/app.sass'
-import SeparatorDemo from './components/ui/separator'
+import { Anotacoes } from './components/anotacoes'
+import { Lista } from './components/listas'
 
-interface Note {
-  id: string
-  data: Date
-  content: string
-}
-
-export function App(): JSX.Element {
-  const [search, setSearch] = useState('')
-  const [notes, setNotes] = useState<Note[]>(() => {
-    const notesOnStorage: string | null = localStorage.getItem('notes')
-
-    if (notesOnStorage !== null) {
-      return JSON.parse(notesOnStorage)
-    }
-
-    return []
-  })
-
+export function App (): JSX.Element {
   const linha = useRef<HTMLHeadingElement>(null)
   const anotacoes = useRef<HTMLButtonElement>(null)
   const lista = useRef<HTMLButtonElement>(null)
 
-  const onNoteCreated = (content: string): void => {
-    const newNote = {
-      id: crypto.randomUUID(),
-      data: new Date(),
-      content
-    }
+  const [estaSelecionadoLista, setEstaSelecionadoLista] = useState<boolean>(false)
 
-    const notesArry = [newNote, ...notes]
-
-    setNotes(notesArry)
-
-    localStorage.setItem('notes', JSON.stringify(notesArry))
-  }
-
-  const onNoteDeleted = (id: string): void => {
-    const notesArray = notes.filter((note) => {
-      return note.id !== id
-    })
-
-    setNotes(notesArray)
-
-    localStorage.setItem('notes', JSON.stringify(notesArray))
-  }
-
-  const handleSearch = (event: ChangeEvent<HTMLInputElement>): void => {
-    const query = event.target.value
-
-    setSearch(query)
-  }
-
-  console.log(search)
-
-  const filteredNotes =
-    search !== ''
-      ? notes.filter((note) =>
-        note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())
-      )
-      : notes
+  const [estaAlternadoLista, setEstaAlternadoLista] = useState(true)
 
   const posicionadoSelecaoTipo = (tipoSelecao): void => {
     console.log(tipoSelecao)
@@ -94,55 +41,37 @@ export function App(): JSX.Element {
       if (currentLinha != null) {
         currentLinha.style.width = (widthLista - 20) + 'px'
 
-        currentLinha.style.left = (widthLista + 10) + 'px'
+        currentLinha.style.left = (leftLista + 10) + 'px'
         console.log(currentLinha.style.left)
       }
     }
   }
 
-  // useEffect(() => {
-  //   posicionadoSelecaoTipo()
-  // }, [])
-
-  document.addEventListener('mousemove', (event) => {
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-    console.log('Posição do mouse X:', mouseX);
-    console.log('Posição do mouse Y:', mouseY);
-});
+  const toggleVisualization = (): void => {
+    setEstaAlternadoLista(true)
+  }
 
   return (
 
     <div className="contener">
       <div className='contener__selecao'>
         <div className='contener__selecao__tipo'>
-          <button ref={anotacoes} onClick={() => { posicionadoSelecaoTipo('anotacoes') }}>Anotações</button>
-          <button ref={lista} onClick={() => { posicionadoSelecaoTipo('lista') }}>Listas</button>
+          <button ref={anotacoes} onClick={() => {
+            posicionadoSelecaoTipo('anotacoes')
+            toggleVisualization()
+          }}>Anotações</button>
+
+          <button className={`contener__selecao__tipo__visivel ${estaAlternadoLista ? '' : 'contener__selecao__tipo__oculto'}`}
+          ref={lista}
+          onClick={() => { posicionadoSelecaoTipo('lista'), setEstaSelecionadoLista(true) }}>Listas</button>
         </div>
         <div className='div__linha'>
           <h3 className='div__linha__h3' ref={linha}></h3>
         </div>
       </div>
 
-      {/* <form className='w-full'>
-        <input type="text"
-          placeholder='Busque em suas notas...'
-          className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
-          onChange={handleSearch}
-        />
-      </form>
+      {estaSelecionadoLista ? <Lista /> : <Anotacoes /> }
 
-      <div className='h-px bg-slate-700' />
-
-      <div className='grid grid-cols-1 md:grid-cols-2 grid-cols-3 gap-6 auto-rows-[256px]'>
-        <NewNoteCard onNoteCreated={onNoteCreated} />
-
-        {filteredNotes.map(note => {
-          return (
-            <NoteCard onNoteDeleted={onNoteDeleted} key={note.id} note={note} />
-          )
-        })}
-      </div> */}
     </div>
   )
 }
---
+++ src/components/anotacoes.tsx
@@ -0,0 +1,77 @@
+import React, { type ChangeEvent, useState } from 'react'
+import { NoteCard } from './note-card'
+import { NewNoteCard } from './new-note-card'
+
+interface Note {
+  id: string
+  data: Date
+  content: string
+}
+
+export function Anotacoes (): React.JSX.Element {
+  const [search, setSearch] = useState('')
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage: string | null = localStorage.getItem('notes')
+
+    if (notesOnStorage !== null) {
+      return JSON.parse(notesOnStorage)
+    }
+
+    return []
+  })
+
+  const onNoteCreated = (content: string): void => {
+    const newNote = {
+      id: crypto.randomUUID(),
+      data: new Date(),
+      content
+    }
+
+    const notesArry = [newNote, ...notes]
+
+    setNotes(notesArry)
+
+    localStorage.setItem('notes', JSON.stringify(notesArry))
+  }
+
+  const onNoteDeleted = (id: string): void => {
+    const notesArray = notes.filter((note) => {
+      return note.id !== id
+    })
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>): void => {
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  console.log(search)
+
+  const filteredNotes =
+        search !== ''
+          ? notes.filter((note) =>
+            note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+          )
+          : notes
+
+  return (
+        <>
+            <div className='h-px bg-slate-700' />
+
+            <div className='grid grid-cols-1 md:grid-cols-2 grid-cols-3 gap-6 auto-rows-[256px]'>
+                <NewNoteCard onNoteCreated={onNoteCreated} />
+
+                {filteredNotes.map(note => {
+                  return (
+                        <NoteCard onNoteDeleted={onNoteDeleted} key={note.id} note={note} />
+                  )
+                })}
+            </div>
+        </>
+  )
+}
